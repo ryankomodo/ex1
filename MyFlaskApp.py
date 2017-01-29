280d8bef2e512908cfd8f0696c25c7956833ceb7
ryankomodo <ryan_gong@hotmail.com>
2017-01-30 00:09:22 +0800

    Initial commit

---
+++ MyFlaskApp.py
@@ -0,0 +1,33 @@
+#import  flask
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask (__name__)
+
+# create URL & function mapping for root or /
+@app.route("/")
+def index():
+    return "hello from Flask!!!"
+
+# create another mapping for /hello
+@app.route("/hello")
+def hello():
+    myName ="Ryan"
+    return "hello again !!" + myName
+
+#create mapping for /myprofile
+@app.route("/myprofile")
+def showmyprofile():
+    return render_template("MyProfileForm.html")
+
+# create a mapping for /addprofile
+@app.route("/addprofile")
+def addprofile():
+    myname = request.args.get("myname")
+    state_of_residence = request.args.get("state_of_residence")
+    return render_template("MyProfile.html",myname=myname,state_of_residence=state_of_residence)
+
+if __name__ == "__main__":
+    app.run()
+
